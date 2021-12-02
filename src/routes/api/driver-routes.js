eeb1615ef9a9c53f9f258584203e8c43afcc3af1
jamesj995 <mrjam7@gmail.com>
2021-12-01 18:31:27 -0600

    fs functions to edit data working

--- src/routes/api/driver-routes.js
+++ src/routes/api/driver-routes.js
@@ -1,19 +1,31 @@
-let drivers = require('./drivers.json');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+let drivers = require('../../data/drivers.json');
 
-app.get('/drivers', (req, res) => {
-  fs.readFile(__dirname + '/' + 'drivers.json', 'utf8', (err, data) => {
-    res.end(data);
-  });
+router.get('/', (req, res) => {
+  try {
+    fs.readFile(__dirname +  "/" + "../../data/drivers.json", 'utf8', (err, data) => {
+      res.status(200).end(data);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-app.post('/drivers', async (req, res) => {
+router.post('/', (req, res) => {
   let driver = req.body;
   drivers.push(driver);
-  await fs.writeFile('drivers.json', JSON.stringify(drivers), (err) => {
-    // Checking for errors
-    if (err) throw err;
-  });
-  await fs.readFile(__dirname + '/' + 'drivers.json', 'utf8', (err, data) => {
-    res.end(data);
-  });
+  try {
+    fs.writeFile(__dirname + "/" + "../../data/drivers.json",
+      JSON.stringify(drivers),
+      () => {
+        res.status(200).json(drivers);
+      }
+    );
+  } catch (err) {
+    res.status(400).end(err);
+  }
 });
+
+module.exports = router;
