2b442d0c8bef9aee57148b2ec9793870de101ca7
JamesJ995 <mrjam7@gmail.com>
2021-12-03 18:15:43 -0600

    trips put working

--- src/routes/api/trip-routes.js
+++ src/routes/api/trip-routes.js
@@ -17,9 +17,40 @@ router.get('/', (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  try{
+    fs.readFile(__dirname + '/' + '../../data/trips.json')
+  } catch (err){
+    res.status(400).json(err);
+  }
+})
+
 router.post('/', (req, res) => {
   let trip = req.body;
   trips.push(trip);
+  try {
+    fs.writeFile(
+      __dirname + '/' + '../../data/trips.json',
+      JSON.stringify(trips),
+      () => {
+        res.status(200).json(trips);
+      }
+    );
+  } catch (err) {
+    res.status(400).end(err);
+  }
+});
+
+router.put('/', (req, res) => {
+  const currentID = req.body.id;
+  const newVibe = req.body.current_vibe;
+  for(let i=0; i < trips.length; i++){
+    if(trips[i].id == currentID){
+      console.log(newVibe);
+      trips[i].current_vibe = newVibe;
+      console.log(trips[i].current_vibe);
+    }
+  }
   try {
     fs.writeFile(
       __dirname + '/' + '../../data/trips.json',
